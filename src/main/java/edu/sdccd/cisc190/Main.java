5d116e4d7bb35fc3fa157313eb47457fc8205c0c
Andrew Huang <ahuang@sdccd.edu>
2024-02-08 11:24:27 -0800

    initial commit

---
+++ src/main/java/edu/sdccd/cisc190/Main.java
@@ -0,0 +1,27 @@
+package edu.sdccd.cisc190;
+
+public class Main {
+    /**
+     * Main.main() is the entry point for this app
+     * @param args input parameters
+     */
+    public static void main(String[] args) {
+        int integerDemo = 1;
+        System.out.println(integerDemo);
+
+        long longDemo = 99999999999L;
+        System.out.println(longDemo);
+        System.out.println(longDemo + 200);
+        System.out.println(longDemo - 200);
+        System.out.println(longDemo / 5);
+        System.out.println(longDemo * 25);
+
+//        byte
+//        long
+//        float
+//        double
+
+        System.out.println("hello world!");
+
+    }
+}
