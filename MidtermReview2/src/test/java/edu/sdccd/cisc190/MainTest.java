4f7f888a9f658bed5d627a0227ce10f114f68067
Andrew Huang <ahuang@sdccd.edu>
2024-03-14 09:08:30 -0700

    started MidtermReview2 module

---
+++ MidtermReview2/src/test/java/edu/sdccd/cisc190/MainTest.java
@@ -0,0 +1,20 @@
+package edu.sdccd.cisc190;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    /**
+     * If str1 and str2 are both Strings, which of the following will
+     * correctly test to determine whether str1 is less than str2?
+     */
+    void stringCmpTest() {
+        String str1 = "hello world";
+        String str2 = "world, hello!";
+
+        System.out.println(true);
+
+    }
+}
